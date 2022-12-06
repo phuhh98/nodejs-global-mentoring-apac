d040d016aa41b0506af822d044e55d3a36c56eb4
Phu Huynh <phu_huynh@epam.com>
2022-12-06 17:38:10 +0700

    use stream.pipeline() instead of readable.pipe()

--- task2.js
+++ task2.js
@@ -1,33 +1,31 @@
 const fs = require('fs');
 const path = require('path');
+const stream = require('stream');
 const csv = require('csvtojson');
 
 const csvFolder = 'csv';
-const csvFileName = 'nodejs-hw1-ex1.csv';
-// const csvFileName = 'MOCK_DATA.csv';
+// const csvFileName = 'nodejs-hw1-ex1.csv';
+const csvFileName = 'MOCK_DATA.csv';
 const outputFileName = 'result.txt';
 
 const csvFilePath = path.resolve(__dirname, csvFolder, csvFileName);
 const outputPath = path.resolve(__dirname, outputFileName);
 
 const fileReader = fs.createReadStream(csvFilePath, {
-  highWaterMark: 1 * 1024,
+  //   highWaterMark: 1 * 1024,
 });
 const fileWriter = fs.createWriteStream(outputPath);
-
-fileReader.on('error', err => {
-  console.error(`fileReader error\n${err}`);
-});
-fileWriter.on('error', err => {
-  console.error(`fileWriter error\n${err}`);
-});
 
 const csvConverter = csv({
   noheader: false,
   //   headers: ['book', 'author', 'amount', 'price'],
 });
-csvConverter.on('error', err => {
-  console.error(`csvConverter error\n${err}`);
-});
 
-fileReader.pipe(csvConverter).pipe(fileWriter);
+// fileReader.pipe(csvConverter).pipe(fileWriter);
+stream.pipeline(fileReader, csvConverter, fileWriter, err => {
+  if (err) {
+    console.error('Pipeline error:', err);
+  } else {
+    console.log('converting is complete');
+  }
+});
